e3667cf106da5036312819d1a2c33fe4f424533b
Peter <peterhahn@Peters-MBP.fios-router.home>
2019-06-14 15:47:34 -0400

    time

---
+++ scripts.js
@@ -0,0 +1,124 @@
+// html5media enables <video> and <audio> tags in all major browsers
+// External File: http://api.html5media.info/1.1.8/html5media.min.js
+
+
+// Add user agent as an attribute on the <html> tag...
+// Inspiration: http://css-tricks.com/ie-10-specific-styles/
+var b = document.documentElement;
+b.setAttribute('data-useragent',  navigator.userAgent);
+b.setAttribute('data-platform', navigator.platform );
+
+// HTML5 audio player + playlist controls...
+// Inspiration: http://jonhall.info/how_to/create_a_playlist_for_html5_audio
+// Mythium Archive: https://archive.org/details/mythium/
+jQuery(function ($) {
+    var supportsAudio = !! document.createElement('audio').canPlayType;
+    if (supportsAudio) {
+        var index = 0,
+            playing = false;
+        mediaPath = './songs/',
+        extension = '',
+        tracks = [{
+            "track": 1,
+            "name": "1. I DON'T WANT TO GO AND SEE THE SUN",
+            "length": "04:16",
+            "file": "Sun"
+        }, {
+            "track": 2,
+            "name": "2. TRAFFIC LIGHTS",
+            "length": "04:37",
+            "file": "Traffic"
+        }, {
+            "track": 3,
+            "name": "3. 8 BIT NEMESIS",
+            "length": "01:50",
+            "file": "8Bit"
+        }, {
+            "track": 4,
+            "name": "4. MICROPHONE REBEL SOLO",
+            "length": "03:01",
+            "file": "Microphone"
+        }, {
+            "track": 5,
+            "name": "5. (INTERLUDE - BINTANG)",
+            "length": "02:53",
+            "file": "Bintang"
+        }, {
+            "track": 6,
+            "name": "6. ENEMY SONG",
+            "length": "03:49",
+            "file": "Enemy"
+        }, {
+            "track": 7,
+            "name": "7. PROGRAM",
+            "length": "02:36",
+            "file": "Program"
+        }],
+        trackCount = tracks.length,
+        npAction = $('#npAction'),
+        npTitle = $('#npTitle'),
+        audio = $('#audio1').bind('play', function () {
+            playing = true;
+            npAction.text('Now Playing...');
+        }).bind('pause', function () {
+            playing = false;
+            npAction.text('Paused...');
+        }).bind('ended', function () {
+            npAction.text('Paused...');
+            if ((index + 1) < trackCount) {
+                index++;
+                loadTrack(index);
+                audio.play();
+            } else {
+                audio.pause();
+                index = 0;
+                loadTrack(index);
+            }
+        }).get(0),
+        btnPrev = $('#btnPrev').click(function () {
+            if ((index - 1) > -1) {
+                index--;
+                loadTrack(index);
+                if (playing) {
+                    audio.play();
+                }
+            } else {
+                audio.pause();
+                index = 0;
+                loadTrack(index);
+            }
+        }),
+        btnNext = $('#btnNext').click(function () {
+            if ((index + 1) < trackCount) {
+                index++;
+                loadTrack(index);
+                if (playing) {
+                    audio.play();
+                }
+            } else {
+                audio.pause();
+                index = 0;
+                loadTrack(index);
+            }
+        }),
+        li = $('#plList li').click(function () {
+            var id = parseInt($(this).index());
+            if (id !== index) {
+                playTrack(id);
+            }
+        }),
+        loadTrack = function (id) {
+            $('.plSel').removeClass('plSel');
+            $('#plList li:eq(' + id + ')').addClass('plSel');
+            npTitle.text(tracks[id].name);
+            index = id;
+            audio.src = mediaPath + tracks[id].file + extension;
+        },
+        playTrack = function (id) {
+            loadTrack(id);
+            audio.play();
+        };
+        extension = audio.canPlayType('audio/mpeg') ? '.mp3' : audio.canPlayType('audio/ogg') ? '.ogg' : '';
+        loadTrack(index);
+    }
+});
